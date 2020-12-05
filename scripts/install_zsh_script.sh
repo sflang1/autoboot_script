cd7e63930895dcba2ea2fa44ab9e605d54c28dbf
Sebastián Landínez <slandinezg@gmail.com>
2020-12-05 02:51:10 -0500

    added input confirmations for each step and updated some steps of the installers

--- scripts/install_zsh_script.sh
+++ scripts/install_zsh_script.sh
@@ -1,36 +1,80 @@
 #! /bin/bash
 
+# Colors
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 # Essentials
 
-sudo apt-get update
-sudo apt-get -y install aptitude
+echo "Installing essential packages for development...."
 
-sudo apt-get -y install build-essential curl wget openssl libssl-dev software-properties-common
+sudo apt update
+sudo apt -y install build-essential curl wget openssl libssl-dev software-properties-common
 
-sudo aptitude -y install python-software-properties
+echo "${GREEN}Packages installed successfully!${NC}"
 
 # Java Development Kit
-sudo add-apt-repository ppa:linuxuprising/java
-sudo aptitude update
+read -p "Would you like to install the Java Development Kit version 11? [yes/no] " install_jdk
 
-sudo aptitude -y install oracle-java11-installer
+if [ "$install_jdk" = "" ] || [ "$install_jdk" = "yes" ] || [ "$install_jdk" = "y" ]; then
+    echo "Installing jdk 11 from https://adoptopenjdk.net ..."
+    export CODENAME="$(cat /etc/os-release | grep UBUNTU_CODENAME | cut -d = -f 2)"
+    sudo apt -y install wget apt-transport-https gnupg
+    wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+    echo "deb https://adoptopenjdk.jfrog.io/adoptopenjdk/deb $CODENAME main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
+    sudo apt update
+    sudo apt -y install adoptopenjdk-11-hotspot
+
+    if [ "$?" = 0 ]; then
+        echo "${GREEN}Java Development Kit installed successfully!${NC}"
+    else
+        echo "${RED}Java Development Kit could not installed${NC}"
+    fi
+fi
 
 # Installing and configuring git
-sudo aptitude -y install git
-read -p "Introduzca el nombre completo del usuario del equipo: " name
-git config --global user.name "$name"
-read -p "Introduzca el correo electrónico del usuario del equipo: " email
-git config --global user.email "$email"
+read -p "Would you like to install and configure Git? [yes/no] " install_git
+
+if [ "$install_git" = "" ] || [ "$install_git" = "yes" ] || [ "$install_git" = "y" ]; then
+    echo "Installing git..."
+    
+    sudo apt -y install git
+
+    if [ "$?" = 0 ]; then
+        echo "${GREEN}Git installed successfully!${NC}"
+
+        echo -n "${YELLOW}Do you want to configure the global name and email for Git (it will create an SSH key)? [yes/no] ${NC}"
+        read configure_git
+
+        if [ "$configure_git" = "" ] || [ "$configure_git" = "yes" ] || [ "$configure_git" = "y" ]; then
+            read -p "Introduce the device user's full name: " name
+            git config --global user.name "$name"
+            read -p "Introduce the device user's email: " email
+            git config --global user.email "$email"
+            echo "${GREEN}Global configurations set successfully!${NC}"
+            
+            echo "Creating and ssh key"
+            ssh-keygen -t rsa -b 4096 -C "$email"
+            echo "${YELLOW}You can later add your new SSH key to the agent${NC}"
+        fi
+    else
+        echo "${RED}Git could not installed${NC}"
+    fi
+fi
 
 # Installing ZSH
-sudo aptitude -y install zsh
+echo "Installing zsh, a bash interpreter for adding capabilities to bash"
+sudo apt -y install zsh
 # Install oh-my-zsh, a plugin manager for zsh
 curl -L http://install.ohmyz.sh | sh
-
+ 
 # Set the default shell to zsh
+echo "Setting your default interpreter in the terminal to ZSH. Please, introduce your password"
 chsh -s /usr/bin/zsh
-
-echo 'Installing fasd'
+ 
+echo "Installing fasd for quickly surfing between folders"
 # Installing fasd
 cd ~
 git clone https://github.com/clvv/fasd.git
--- scripts/run_with_zsh.sh
+++ scripts/run_with_zsh.sh
@@ -1,5 +1,10 @@
 #!/bin/zsh
-# ========== Esta parte con ZSH
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+echo "Installing oh my zsh plugins..."
 cd ~/.oh-my-zsh/custom/plugins
 
 # Add the syntax highlighting
@@ -9,62 +14,82 @@ git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 # Find the line number of the plugins line
 export line_number=$(grep -nr "plugins=(git" ~/.zshrc | cut -d : -f 1)
 # Replace this line.
-sed -i "$(echo $line_number)s/.*/plugins=(git bundler brew fasd zeus gem rails ruby npm node nanoc history-substring-search zsh-syntax-highlighting)/" ~/.zshrc
+sed -i "$(echo $line_number)s/.*/plugins=(git bundler docker docker-compose fasd gem rails ruby npm node history-substring-search zsh-syntax-highlighting)/" ~/.zshrc
 
 # Fasd configuration
 echo 'eval "$(fasd --init auto)"' >> ~/.zshrc
 
-# Install rbenv
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-cd ~/.rbenv && src/configure && make -C src
-echo "# Rbenv configuration" >> ~/.zshrc
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(rbenv init -)"' >> ~/.zshrc
-# Rbenv build
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo "${GREEN}Zsh plugins installed successfully!${NC}"
 
-# Install tkinter
-sudo apt-get install python3-tk tk-dev
+vared -p 'Would you like to install rbenv (Simple Ruby version manager)? [yes/no] ' -c install_rbenv
 
-# Install pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo "# Python configuration" >> ~/.zshrc
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
-echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+if [[ "$install_rbenv" = ""  || "$install_rbenv" = "yes" || "$install_rbenv" = "y" ]]; then
+    # Install rbenv
+    git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+    cd ~/.rbenv && src/configure && make -C src
+    echo "# Rbenv configuration" >> ~/.zshrc
+    echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
+    echo 'eval "$(rbenv init -)"' >> ~/.zshrc
+    # Rbenv build
+    git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
 
-# Install nodenv
-git clone https://github.com/nodenv/nodenv.git ~/.nodenv
-cd ~/.nodenv && src/configure && make -C src
-echo "# Rbenv configuration" >> ~/.zshrc
-echo 'export PATH="$HOME/.nodenv/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(nodenv init -)"' >> ~/.zshrc
-# Add nodenv binaries
-echo "[[ \$(nodenv version) =~ '([[:digit:]]+\.)+[[:digit:]]+' ]]; export current_version=\$MATCH" >> ~/.zshrc
-echo 'export PATH="$HOME/.nodenv/versions/$current_version/bin:$PATH"' >> ~/.zshrc
-# Nodenv build
-mkdir -p "$(nodenv root)"/plugins
-git clone https://github.com/nodenv/node-build.git $(nodenv root)/plugins/node-build
+vared -p 'Would you like to install pyenv (Simple Python version manager)? [yes/no] ' -c install_pyenv
 
-# Install pgvm
-curl -s -L https://raw.github.com/guedes/pgvm/master/bin/pgvm-self-install | bash
-# Postgres manager conf
-echo "# Pgvm configuration" >> ~/.zshrc
-echo 'source $HOME/.pgvm/pgvm_env' >> ~/.zshrc
+if [[ "$install_pyenv" = ""  || "$install_pyenv" = "yes" || "$install_pyenv" = "y" ]]; then
+    # Install pyenv
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    echo "# Python configuration" >> ~/.zshrc
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+    echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> ~/.zshrc
+    echo 'eval "$(pyenv init -)"' >> ~/.zshrc
+fi
 
-# Install phantomenv
-git clone -b v0.0.10 https://github.com/boxen/phantomenv.git ~/.phantomenv
-echo "#Phantomenv configuration" >> ~/.zshrc
-echo 'export PATH="$HOME/.phantomenv/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(phantomenv init -)"' >> ~/.zshrc
+vared -p 'Would you like to install nodenv (Simple Node version manager)? [yes/no] ' -c install_nodenv
 
-# Install postgres
-sudo apt install postgresql postgresql-contrib
+if [[ "$install_nodenv" = ""  || "$install_nodenv" = "yes" || "$install_nodenv" = "y" ]]; then
+    # Install nodenv
+    git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+    cd ~/.nodenv && src/configure && make -C src
+    echo "# Rbenv configuration" >> ~/.zshrc
+    echo 'export PATH="$HOME/.nodenv/bin:$PATH"' >> ~/.zshrc
+    echo 'eval "$(nodenv init -)"' >> ~/.zshrc
+    # Add nodenv binaries
+    echo "[[ \$(nodenv version) =~ '([[:digit:]]+\.)+[[:digit:]]+' ]]; export current_version=\$MATCH" >> ~/.zshrc
+    echo 'export PATH="$HOME/.nodenv/versions/$current_version/bin:$PATH"' >> ~/.zshrc
+    # Nodenv build
+    mkdir -p ~/.nodenv/plugins
+    git clone https://github.com/nodenv/node-build.git ~/.nodenv/plugins/node-build
+fi
 
+
+vared -p 'Would you like to install postgresql? [yes/no] ' -c install_postgres
+
+if [[ "$install_postgres" = ""  || "$install_postgres" = "yes" || "$install_postgres" = "y" ]]; then
+    # Install postgres
+    sudo apt -y install postgresql postgresql-contrib
+fi
+
+vared -p 'Would you like to install docker and docker compose? [yes/no] ' -c install_docker
+
+if [[ "$install_docker" = ""  || "$install_docker" = "yes" || "$install_docker" = "y" ]]; then
+    sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
+
+    # Installing docker compose
+    sudo apt -y py-pip python-dev libffi-dev openssl-dev gcc libc-dev make.
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+echo "${GREEN}All packages were installed successfully! Please, close your session and reopen it for the terminal changes to take place${NC}"
 
 # Octave
 # Install required and suggested packages
-sudo apt-get install -y gfortran libpcre3-dev libblas-dev liblapack-dev libreadline-dev
-sudo apt-get install -y libqhull-dev libhdf5-serial-dev libfftw3-dev libglpk-dev libcurl4-gnutls-dev libsndfile1-dev portaudio19-dev libgraphicsmagick++1-dev libgl1-mesa-dev libgl1-mesa-dev libfreetype6-dev 	libfontconfig1-dev 	libosmesa6-dev 	libgl2ps-dev libqt4-dev libqtcore4  libqtwebkit4 libqt4-network libqtgui4 libqt4-opengl-dev libfltk1.3-dev
-sudo apt-get install -y libqrupdate-dev	libsuitesparse-dev libarpack2-dev gnuplot-x11
-sudo apt-get install -y libxft-dev libqscintilla2-dev
+# sudo apt install -y gfortran libpcre3-dev libblas-dev liblapack-dev libreadline-dev
+# sudo apt install -y libqhull-dev libhdf5-serial-dev libfftw3-dev libglpk-dev libcurl4-gnutls-dev libsndfile1-dev portaudio19-dev libgraphicsmagick++1-dev libgl1-mesa-dev libgl1-mesa-dev libfreetype6-dev 	libfontconfig1-dev 	libosmesa6-dev 	libgl2ps-dev libqt4-dev libqtcore4  libqtwebkit4 libqt4-network libqtgui4 libqt4-opengl-dev libfltk1.3-dev
+# sudo apt install -y libqrupdate-dev	libsuitesparse-dev libarpack2-dev gnuplot-x11
+# sudo apt install -y libxft-dev libqscintilla2-dev
